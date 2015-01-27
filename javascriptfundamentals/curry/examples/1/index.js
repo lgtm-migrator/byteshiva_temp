2b064a37774a5ee6fee6d701f348f336687647c4
bytes shiva <bytes.shiva@gmail.com>
2015-01-27 20:57:47 +0530

    Curry Example 1

---
+++ javascriptfundamentals/curry/examples/1/index.js
@@ -0,0 +1,19 @@
+var baby = function(a) {
+	return function(b) {
+		var result = 'I love '.concat(a).concat(' and ').concat(b);
+	return result;
+	};
+};
+
+var babyInstance = baby('Indian Baby');
+
+babyInstance('Asian Babies');
+babyInstance('African Babies');
+babyInstance('Australian Babies');
+
+var log = function(fn) {
+	return console.log.call(this,fn);
+}
+
+log(babyInstance('Asian Babies'));
+log(babyInstance('African Babies'));
