a6f839460be58e55d36203ef23992547ab83741d
bytes shiva <bytes.shiva@gmail.com>
2015-01-28 17:50:40 +0530

    moving stack implemtation to seperate Library

---
+++ algorithms/javascript/stack/app/test.js
@@ -0,0 +1,23 @@
+var Stack = require("../libs/index.js");
+
+console.log(typeof(Stack()));
+var s = new Stack();
+s.push("Siva");
+s.push("Subraman");
+s.push("Brama");
+console.log("length: " + s.length());
+console.log(s.peek());
+
+var popped = s.pop();
+console.log("The popped element is: " + popped);
+console.log(s.peek());
+
+s.push("Siddhu");
+console.log(s.peek());
+s.clear();
+
+console.log("length: " + s.length());
+console.log(s.peek());
+
+s.push("niyam");
+console.log(s.peek());
