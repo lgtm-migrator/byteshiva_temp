4b59128f27ee4bde537c1ad0d4199be14947de06
bytes shiva <bytes.shiva@gmail.com>
2015-01-28 17:22:32 +0530

    testing stack implementation

--- algorithms/javascript/stack/index.js
+++ algorithms/javascript/stack/index.js
@@ -28,3 +28,23 @@ function clear() {
 	this.top = 0;
 }
 
+var s = new Stack();
+s.push("Siva");
+s.push("Subraman");
+s.push("Brama");
+console.log("length: " + s.length());
+console.log(s.peek());
+
+var popped = s.pop();
+console.log("The popped element is: " + popped);
+console.log(s.peek());
+
+s.push("Siddhu");
+console.log(s.peek());
+s.clear();
+
+console.log("length: " + s.length());
+console.log(s.peek());
+
+s.push("niyam");
+console.log(s.peek());
