6be89a041bf0d1a017d8cd5ecb94553f4a2f5c1f
= <=byteshiva@gmail.com>
2015-02-02 22:42:18 +0530

    added phantomjs as browser and added addCommand to execute external commands

--- selenium-nodejs/autosubmitwebsite/index.js
+++ selenium-nodejs/autosubmitwebsite/index.js
@@ -3,37 +3,51 @@
 var webdriverio = require('webdriverio'),
     options = {
         desiredCapabilities: {
-            browserName: 'firefox'
+            browserName: 'phantomjs'
         }
     },
     emailId  = process.argv[2],
-    password = process.argv[3];
+    password = process.argv[3],
+    client = webdriverio.remote(options);
+
+    client.addCommand("navigateProfilePage", function(customVar, cb) {
+      return this.click("#leftNav_updateProfile")
+      .click("#leftNav_updateProfile > ul:nth-child(1) > li:nth-child(1) > a:nth-child(2)",function() {
+        cb();
+        console.log(customVar);
+      });
+    });
 
 function frontPageLogin()  {
-    return webdriverio
-    .remote(options)
+    return client
     .init()
     .once('countme', function(e) {
       console.log('elements were found');
     })
     .url('http://www.naukri.com')
-    .click('*[id="login_Layer"]')
-    .log('client')
+    .saveScreenshot("./loginPageSnapShot.png", function(err,screenshot, res){
+      console.log(err);
+    })
+    .getTitle()
+    .getSource()
+    .pause(3000)
+    .click("#login_Layer")
+    // .click('*[id="login_Layer"]')
+    // .log('client')
     .setValue('*[id="eLogin"]', emailId)
     .setValue('*[id="pLogin"]',password)
     .click("div.row:nth-child(13) > button:nth-child(1)");
 }
 
 function loginPage()  {
-    return webdriverio
-    .remote(options)
+    return client
     .init()
     .once('countme', function(e) {
       console.log('elements were found');
     })
     .url('http://login.naukri.com')
     .click('*[id="login_Layer"]')
-    .log('client','ALL')
+    // .log('client','ALL')
     .click('//*[@id="emailRad"]')
     .pause(10000)
     .saveScreenshot("./loginPageSnapShot.png", function(err,screenshot, res){
@@ -53,31 +67,40 @@ var frontPageln = frontPageLogin();
 
       frontPageln
       .pause(3000)
-      .click("#leftNav_updateProfile")
-      .click("#leftNav_updateProfile > ul:nth-child(1) > li:nth-child(1) > a:nth-child(2)")
+      .navigateProfilePage('a custum value', function(err, result) {
+
+      })
       .pause(2000)
-      .setValue("#summary","Team Lead with 6+ experience in USA and overall 13+ Experience working in Java8 Nodejs Backbonejs Javascript selenium")
+      .setValue("#summary","Team Lead with 6+ experience in USA and overall 13+ Experience working in Java8 Nodejs Backbonejs Javascript Selenium Underscorejs AWS JQuery AngularJS")
+      .pause(2000)
       .getText("#summary", function(err, text){
         console.log(text);
       })
-      .saveScreenshot("./summarySnapShot.png", function(err,screenshot, res){
+      .saveScreenshot("./summarySnapShot1.png", function(err,screenshot, res){
         console.log(err);
       })
       .click(".w150bt", function(err,res){
         // console.log(res);
       })
+      .navigateProfilePage('a custum value', function(err, result) {
+          this.getText("#summary", function(err, text) {
+              console.log(text);
+          });
+      })
       .pause(3000)
       .back()
       .refresh()
-      .log('server')
-      .log('driver','ALL')
+      // .log('server')
+      // .log('driver','ALL')
       .emit('countme', "cnt")
       .pause(1000)
       .title(function(err, res) {
           console.log('Title was: ' + res.value);
       })
-      .end();
+      .session('delete')
+      .endAll();
 }
 
 
 updateProfilePage();
+// frontPageLogin();
