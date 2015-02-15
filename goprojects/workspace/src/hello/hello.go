1046e04f541da94c90cc420abacbadb2bdbfdb05
byteshiva <byteshiva@gmail.com>
2015-02-15 22:34:27 +0530

    added new methods in test go file

--- goprojects/workspace/src/hello/hello.go
+++ goprojects/workspace/src/hello/hello.go
@@ -11,10 +11,20 @@ func formatName1(name string) (string, int) {
 	return name, len(name)
 }
 
+func testSlices() {
+	p := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println("p ==", p)
+
+	for i := 0; i < len(p); i++ {
+		fmt.Printf("p[%d] == %d\n", i, p[i])
+	}
+}
+
 func main() {
 	fmt.Println("Greetings, Fellow go-developers ")
 	name, size := formatName1("Siva Subra")
 	// name, _ = formatName("Siva Chii")
 	fmt.Println(name, size)
 	fmt.Println(formatName("Siva"))
+	testSlices()
 }
