3ab4e9420969f01faced232466282787c5524c02
Shiva <byteshiva@gmail.com>
2015-02-24 21:28:05 +0530

    verifying structs pointer value

--- goprojects/workspace/src/structs/main.go
+++ goprojects/workspace/src/structs/main.go
@@ -9,4 +9,20 @@ type person struct {
 
 func main() {
 	fmt.Println(person{"Siva", 100})
+
+	fmt.Println(person{name: "Siva", age: 500})
+
+	fmt.Println(person{name: "Subra", age: 560})
+
+	fmt.Println(person{name: "Chi"})
+
+	s := person{name: "Maniyam", age : 760}
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 510
+	fmt.Println(sp.age)
+	fmt.Println(s.age)
 }
