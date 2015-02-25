703b1273789d76fd5cfbdf1b002befa54adb9124
Shiva <byteshiva@gmail.com>
2015-02-26 00:45:03 +0530

    added example for channel synchronization and executable

---
+++ goprojects/workspace/src/channelsynchronization/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func worker(done chan bool) {
+	fmt.Println("working...")
+	time.Sleep(time.Second)
+	fmt.Println("Done")
+	done <- true
+}
+
+func main() {
+
+	done := make(chan bool, 1)
+	go worker(done)
+	<- done
+}
