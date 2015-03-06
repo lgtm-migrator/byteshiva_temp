8b2a59f7bfb39f88a7e2ae644fde6fbb534dbf99
Shiva <byteshiva@gmail.com>
2015-03-06 19:36:39 +0530

    added comments to find the # of times the function executes

--- algorithms/javascript/factorial/index.js
+++ algorithms/javascript/factorial/index.js
@@ -1,8 +1,13 @@
+var i = 1;
 function factorial(n) {
+	//console.log("Number of times " , i);
+	i = i + 1;
 	if(n == 0)
 		return 1;
-	else 
+	else {
+		console.log("Number of times called " , i, n);
 		return (n * factorial(n - 1));
+	}
 }
 
-console.log(factorial(5));
+console.log(factorial(10));
