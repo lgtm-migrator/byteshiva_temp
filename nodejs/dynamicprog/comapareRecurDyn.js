e773579671a89cf6c279b40f77956aa64adb05fc
Shiva <byteshiva@gmail.com>
2015-03-08 20:13:51 +0530

    added example for recurrsive and dynamic prog example

---
+++ nodejs/dynamicprog/comapareRecurDyn.js
@@ -0,0 +1,16 @@
+var dyn = require("./dynfib.js");
+var recurr = require("./fibonacci.js");
+
+
+
+var start = new Date().getTime();
+z = recurr(45);
+console.log(z);
+var stop = new Date().getTime();
+console.log("recursive time - " + (stop-start) + "milliseconds");
+
+var start1 = new Date().getTime();
+y = dyn(45);
+console.log(y);
+var stop1 = new Date().getTime();
+console.log("dynamic programming time - " + (stop1-start1) + "milliseconds");
--- nodejs/dynamicprog/fibonacci.js
+++ nodejs/dynamicprog/fibonacci.js
@@ -1,13 +1,10 @@
 function recurFib(n) {
-	var space = " ";
 	if(n < 2) {
-		console.log(space, "recur times called : " , n);
 		return n;
 	} else {
-		console.log("recur times called : ", n);
 		return recurFib(n-1) + recurFib(n-2);
 		
 	}
 }
 
-console.log(recurFib(10));
+module.exports = recurFib;
