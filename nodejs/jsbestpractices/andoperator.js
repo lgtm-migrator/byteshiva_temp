8d0f93a068d42bb55489730951046218dd292b72
Shiva <byteshiva@gmail.com>
2015-03-09 22:29:56 +0530

    example for and operator - short circuiting

---
+++ nodejs/jsbestpractices/andoperator.js
@@ -0,0 +1,27 @@
+var strength = true;
+var fear = false;
+var pack = {
+  food: [ "carrot",
+          "mystery meat",
+          "apple",
+          "crust of bread",
+          "spicy dried sausage",
+          "carrot",
+          "wedge of sharp cheese",
+          "jug of milk",
+          "mystery meat",
+          "carrot"
+  ],
+  addFood: function(foodItem){
+    this.food = this.food || [];
+    this.food.push(foodItem);
+  },
+  enoughFood:function(amount){
+    return(this.food.length >= amount);
+  }
+};
+
+function(){var foods= pack.food; for (index in foods) {pack.addFood(foods[index]);}}()
+var surviveThisTrial = strength && !fear && pack.enoughFood(10);
+
+console.log(surviveThisTrial);
