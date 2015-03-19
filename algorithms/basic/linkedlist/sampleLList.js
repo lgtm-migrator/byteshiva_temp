037ed3d89e5f06d80ad8dc9cde8a40ce5d7e5622
Siva <byteshiva@gmail.com>
2015-03-19 18:06:37 +0530

    comment remove from linked list example and add new file to test linked list example

---
+++ algorithms/basic/linkedlist/sampleLList.js
@@ -0,0 +1,7 @@
+var LList = require('./LList.js');
+
+var cities = new LList();
+cities.insert("Conway","head");
+cities.insert("Russellville","Conway");
+cities.insert("Alma","Russellville");
+cities.display();
